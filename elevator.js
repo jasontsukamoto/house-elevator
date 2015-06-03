f8540843e54acffae2804732be3579535e0d23d0
Jason Tsukamoto <jasontsukamoto@gmail.com>
2015-06-03 12:14:21 -1000

    complete elevator.js

--- elevator.js
+++ elevator.js
@@ -2,5 +2,32 @@
 'use strict';
 
 module.exports = {
+  goto : function(current, destination) {
+    var levels = 4;
 
+    //error check -need to check if they are not a number then return 0
+   if(current > levels -1 || destination > levels - 1 || typeof current !== 'number' || typeof destination !== 'number') {
+      return 0;
+    } else {
+      //makes the elevator move up and down
+      if (current === 0) {
+        return current + destination;
+      } else if (current !== 0 && current > destination) {
+        return destination - current;
+      } else if (current !== 0 && current < destination) {
+        return destination - current;
+      } else if (current === destination) {
+        return 0;
+      }
+    }
+
+
+
+
+
+
+
+  //closes function
+  }
 };
+
